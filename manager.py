e581a48b7fcfbb85ce2988f00e3d99a773f9af0c
David-396 <david.eini12@gmail.com>
2025-09-02 12:21:05 +0300

    add logging

--- manager.py
+++ manager.py
@@ -1,7 +1,9 @@
+import logging
 from data_loader import Loader
 from es import ElasticConnector
 from process import Process
 
+logging.basicConfig(level=logging.INFO)
 
 class Manager:
     def __init__(self,
@@ -33,21 +35,32 @@ class Manager:
 
     # main func
     def run(self):
+
+        logging.info('starting manager..')
+
         # create a new index if not exist
         if not self.client.es_client.indices.exists(index=self.index_name):
+            logging.info(f'"{self.index_name}" index not exist in es - creating..')
             self.client.create_index(index_name=self.index_name, mapping=self.index_mapping, settings=self.index_settings)
 
         # insert all the docs
+        logging.info(f'indexing data to es. data example - {self.data[0]}')
         self.client.insert_docs(index_name=self.index_name, docs=self.data)
 
         # get the whole docs to process them
+        logging.info('get all docs to process..')
         all_docs = self.es_obj.search(index=self.index_name, size=10000)["hits"]["hits"]
 
         # update the docs by a function - process #1
+        logging.info(f'updating "{self.sentiment_col}" field to identify sentiment from "{self.text_col}" field..')
         self.client.update_docs(index_name=self.index_name, docs=all_docs, process_col=self.sentiment_col, callback=self.processor.identify_sentiment)
 
         # update the docs by a function - process #2
+        logging.info(f'updating "{self.weapons_col}" field to identify weapons from "{self.text_col}" field..')
         self.processor.identify_weapons(es_obj=self.client, index_name=self.index_name, searched_col=self.text_col, assigning_col=self.weapons_col)
 
         # delete docs by query
+        logging.info(f'deleting documents by delete query - {self.delete_query}')
         self.client.delete_docs(index_name=self.index_name, query=self.delete_query)
+
+        logging.info('finish to process.')
